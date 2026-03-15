597a5db6bc1b18c8b9873edfe0c50c2914ef34f2
Lexqwe <alexandr1.zet@gmail.com>
2026-03-15 13:26:30 +0500

    ваш комментарий

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,8 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name='list'),
-    path('create', views.article_create, name='create'),
+    path('form', views.article_form, name='form'),
     path('<slug:slug>', views.article_item, name='article_detail'),
+    path('update/<slug:slug>', views.article_update, name='update'),
+    path('delete/<slug:slug>', views.article_delete, name='delete'),
 ]
